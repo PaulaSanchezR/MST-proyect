fd36d7f9f5c40d4e3e1dd6fe008421a3dc37a157
PaulaSanchezR <paulasanchez.restrepo@gmail.com>
2019-03-04 22:09:29 -0500

    finishing style

--- bin/seeds.js
+++ bin/seeds.js
@@ -3,7 +3,9 @@ const Printer = require ('../models/printer');
 
 const dbName = 'mst-proyect';
 mongoose.connect(`mongodb://localhost/${dbName}`);
-
+//     when I run the seeds I need to use this line and on the command line use node seeds.js
+//                  |
+//mongoose.connect(`mongodb://heroku_46vlmzwk:bnbj5ja6j6fju6pcfo98m75jp5@ds123619.mlab.com:23619/heroku_46vlmzwk`);
 const printer=[
     {
         printer     : "TSP100III",
